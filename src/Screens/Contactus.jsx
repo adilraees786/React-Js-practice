36000cc5c268f2df1635be88b635280f72cb3136
adilraees786 <adilraees911@gmail.com>
2025-06-24 16:16:49 +0500

    fix

--- src/Screens/Contactus.jsx
+++ src/Screens/Contactus.jsx
@@ -1,32 +1,3 @@
-// import React, { Component } from 'react';
-// import { Map, GoogleApiWrapper } from 'google-map-react';
-
-// class ContactUs extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <Map
-//           google={this.props.google}
-//           zoom={10}
-//           initialCenter={{
-//             lat: 24.8546842,
-//             lng: 67.0207055
-//           }}
-//           style={{
-//             width: '100%',
-//             height: '100%',
-//             position: 'relative'
-//           }}
-//         />
-//       </div>
-//     );
-//   }
-// }
-
-// export default GoogleApiWrapper({
-//   apiKey: "AIzaSyB9B8fcCKugprNNeE6DlqoPM-OuzUBZ0lE"
-// })(ContactUs);
-
 
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
